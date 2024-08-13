e9a22d3193ccd02c4b2da766295e89f4b4439dd5
hedaguler <127657696+hedaguler@users.noreply.github.com>
2024-08-13 12:22:22 +0300

    08.13.2024

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    //Geneeric classta <tip> belirtmek zorunlu
+    internal class MyList<T>
+    {
+        T[] items;
+
+        //ctor yaz tab tab constructor oluşturdun
+        //constructorla classın ismi aynıdır(MyList)
+        //classı newlediğinde constructorı otomatik çalışır
+        public MyList() 
+        {
+                items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];   //dizinin eleman saysını 1 arttırdık. dizinin eleman sayısında işlem yapabilmek için newlemek gerekiyor.
+            for (int i = 0; i < tempArray.Length; i++) 
+            {
+                items[i] = tempArray[i];    
+            }
+
+            items[items.Length - 1]=item;   //arrayin son elemanı Length-1 demektir.
+        }
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//CLASSLAR 2 YE AYRILIR ->içinde özellik barındıran , operasyon(method) barındıran
+
+using OOP1;
+
+Product product1 = new Product();
+product1.Id = 1;
+product1.CategoryId = 2;
+product1.ProductName = "Masa";
+product1.UnitPrice = 500;
+product1.UnitInStock = 3;
+
+Product product2= new Product {Id = 2 , CategoryId =5, UnitInStock=5 ,ProductName="Kalem" ,UnitPrice=35};
+
+ProductManager productManager =new ProductManager () ;
+productManager.Add(product1);
+Console.WriteLine (product1.ProductName);
+// product1.ProductName = "Masa";   BAŞTA MASA ATAMIŞTIK
+//AMA SONRA ProductManager ICINDE kamera ATADIK outputumuz kamera oldu
+
+
+
+
+
+
+
+
+
+
+//ProductManager productmanager ;
+//String         isim ;
+// PascalCase    camelCase
+//Class          değişkenAdı
+//Classların adları Pascal Case ile yazılır.
+
+
+// referans tiplerinin heapte oluşabilmesi için NEW lenmesi gerekir.
+
+
+// Stack de oluşturulan kısım = new heapte oluşturulan kısım;
